c1169892b42fa2b722502a95f068beb349784480
Safal Neupane <safalneupane@Safals-MacBook-Pro.local>
2023-01-08 21:51:02 +0545

    Initial Commit

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,67 @@
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const fs = require('fs');
+const Tour = require('../../models/tourModel');
+const User = require('../../models/userModel');
+const Review = require('../../models/reviewModel');
+
+dotenv.config({
+  path: './config.env',
+});
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log('DB connection sucessful');
+  });
+
+// const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, 'utf8'));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, 'utf8'));
+// const reviews = JSON.parse(
+// fs.readFileSync(`${__dirname}/reviews.json`, 'utf8')
+// );
+
+//IMPORT DATA INTO THE DATABASE
+
+const importData = async () => {
+  try {
+    // await Tour.create(tours);
+    await User.create(users, { validateBeforeSave: false });
+    // await Review.create(reviews);
+    console.log('Data imported successfully');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+//DELETE ALL DATA FROM DATABASE
+
+const deleteAllData = async () => {
+  try {
+    // await Tour.deleteMany();
+    await User.deleteMany();
+    // await Review.deleteMany();
+    console.log('Data deleted successfully');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteAllData();
+}
+
+console.log(process.argv);
